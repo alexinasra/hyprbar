abc6a68fa9d6906870e2c3b25b9ff397890861e0
Alexei Nasra <alexinasra@gmail.com>
2025-11-23 17:45:01 +0200

    changed clock ui element from text to button

--- src/elements/clock.cpp
+++ src/elements/clock.cpp
@@ -1,7 +1,9 @@
 #include "./clock.hpp"
 #include <chrono>
+#include <hyprtoolkit/element/Rectangle.hpp>
+#include <hyprtoolkit/types/SizeType.hpp>
 #include <iomanip>
-#include <print>
+#include <spdlog/spdlog.h>
 #include <sstream> 
 #include <thread>
 #include <ctime>
@@ -9,31 +11,32 @@
 
 
 Clock::Clock() {
-    clockLabel = Hyprtoolkit::CTextBuilder::begin()
-        ->commence();
-    std::thread tupdateThread([&] {
+     clockLabel = Hyprtoolkit::CButtonBuilder::begin()
+                ->size({Hyprtoolkit::CDynamicSize::HT_SIZE_ABSOLUTE, Hyprtoolkit::CDynamicSize::HT_SIZE_ABSOLUTE, {80, 30}})
+                ->commence();
+     std::thread tupdateThread([&] {
         while (!stopTimeUpdate) {
-            // Get the current time point using std::chrono::system_clock
             auto now = std::chrono::system_clock::now();
             std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
 
-            // Convert to local time structure (thread-safe in C++20, but use std::mutex for general compatibility)
             std::tm localTime = {};
-            localtime_r(&currentTime, &localTime); // POSIX version
+            localtime_r(&currentTime, &localTime);
 
             std::ostringstream timeStream;
             timeStream << std::put_time(&localTime, format.c_str());
             std::string timeStr = timeStream.str();
 
-            // Lock the mutex before writing to the console
             std::lock_guard<std::mutex> lock(timeUpdateMutex);
-            // std::cout << timeStr;
-            // std::cout.flush();
-            clockLabel->rebuild()->text(std::string(timeStr))->commence();
+            clockLabel->rebuild()
+                ->onMainClick([&](auto) {
+                        spdlog::info("todo: Open Calendar widget");
+                })
+                ->label(std::string(timeStr)) 
+                ->commence();
+            clockLabel->setTooltip("SomeText");
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
         }
     });
-    tupdateThread.detach();
- 
+    tupdateThread.detach(); 
 };
 
--- src/elements/clock.hpp
+++ src/elements/clock.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include <hyprtoolkit/element/Button.hpp>
 #include <hyprtoolkit/element/Text.hpp>
 #include <hyprutils/memory/SharedPtr.hpp>
 #include <atomic>
@@ -11,10 +12,10 @@ class Clock {
     public:
         Clock();
         Clock(Hyprutils::Memory::CSharedPointer<Hyprtoolkit::CTextElement> clockLabel);
-        inline Hyprutils::Memory::CSharedPointer<Hyprtoolkit::CTextElement> getLabel() { return clockLabel; }; 
+        inline Hyprutils::Memory::CSharedPointer<Hyprtoolkit::CButtonElement> getLabel() { return clockLabel; }; 
     private:
         std::string format = "%H:%M";
-        Hyprutils::Memory::CSharedPointer<Hyprtoolkit::CTextElement> clockLabel;
+        Hyprutils::Memory::CSharedPointer<Hyprtoolkit::CButtonElement> clockLabel;
         std::mutex timeUpdateMutex;
         std::atomic<bool> stopTimeUpdate;
 
